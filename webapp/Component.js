4ffb53c5504c0086072e4f1ce69b19a9e493de80
Manuel Blechschmidt <manuel.blechschmidt@gmx.de>
2017-12-10 14:52:17 +0000

    * Add upload button

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
-	"incentergy/bccrm/BusinessCardCRM/model/models"
-], function(UIComponent, Device, models) {
+	"incentergy/bccrm/BusinessCardCRM/model/models",
+	"openui5/community/model/firebase/FirebaseModel"
+], function(UIComponent, Device, models, FirebaseModel) {
 	"use strict";
 
 	return UIComponent.extend("incentergy.bccrm.BusinessCardCRM.Component", {
@@ -19,7 +20,26 @@ sap.ui.define([
 		init: function() {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
-
+			var oFBConfig = {
+			    apiKey: "AIzaSyB98nY09BXBnx3n5GVMlw0GD06tiRcq6xg",
+			    authDomain: "businesscardcrm.firebaseapp.com",
+			    databaseURL: "https://businesscardcrm.firebaseio.com",
+			    projectId: "businesscardcrm",
+			    storageBucket: "",
+			    messagingSenderId: "906525357736"
+			};
+			var oComponentModel = new FirebaseModel(null, oFBConfig);
+			// Login to firebase
+			oComponentModel.getFirebasePromise().then(function(firebase){
+			    firebase.auth().signInAnonymously().catch(function(error) {
+			        // Handle Errors here.
+			        var errorCode = error.code;
+			        var errorMessage = error.message;
+			        // Do something. Panic is acceptable
+			        jQuery.sap.log.error(errorCode+" "+errorMessage);
+			    });
+			});
+			this.setModel(oComponentModel);
 			// enable routing
 			this.getRouter().initialize();
 
--- webapp/controller/BusinessCardCRM.controller.js
+++ webapp/controller/BusinessCardCRM.controller.js
@@ -1,9 +1,16 @@
 sap.ui.define([
+	"jquery.sap.global",
 	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+], function(jQuery, Controller) {
 	"use strict";
 
 	return Controller.extend("incentergy.bccrm.BusinessCardCRM.controller.BusinessCardCRM", {
-
+		onAddBusinessCard: function(oEvent) {
+			// creating input on-the-fly
+	        var input = jQuery(document.createElement("input"));
+	        input.attr("type", "file");
+	        // add onchange handler if you wish to get the file :)
+	        input.trigger("click"); // opening dialog
+		}
 	});
 });
